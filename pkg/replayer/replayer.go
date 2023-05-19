c02235b8b573bcb1adcf659925796f23a7c96e47
piraces <raul.piraces@gmail.com>
2023-05-19 21:52:55 +0200

    fix: attempt to fix websockets closing when replaying

--- pkg/replayer/replayer.go
+++ pkg/replayer/replayer.go
@@ -42,9 +42,8 @@ func ReplayEventsToRelays(parameters *ReplayParameters) {
 		parameters.Mutex.Lock()
 		// publish the event to predefined relays
 		for _, url := range parameters.RelaysToPublish {
-			relay, e := nostr.RelayConnect(context.Background(), url)
-			if e != nil {
-				log.Printf("[ERROR] Error while trying to connect with relay '%s': %v", url, e)
+			relay := connectToRelay(url)
+			if relay == nil {
 				continue
 			}
 
@@ -55,7 +54,10 @@ func ReplayEventsToRelays(parameters *ReplayParameters) {
 				if shouldPerformAuthRequest && !tryAuth(relay, *challenge, url, parameters.WaitTimeForRelayResponse, &ev) {
 					continue
 				}
+
+				relay = connectToRelay(url)
 				publishStatus, err := relay.Publish(context.Background(), ev.Event)
+				_ = relay.Close()
 				if err != nil {
 					log.Printf("[INFO] Failed to replay event to %s with error: %v", url, err)
 				}
@@ -114,4 +116,14 @@ func tryAuth(relay *nostr.Relay, challenge string, url string, waitTime int64, e
 		return true
 	}
 	return false
+}
+
+func connectToRelay(url string) *nostr.Relay {
+	relay, e := nostr.RelayConnect(context.Background(), url)
+	if e != nil {
+		log.Printf("[ERROR] Error while trying to connect with relay '%s': %v", url, e)
+		return nil
+	}
+
+	return relay
 }
